456830d7ad0ff7e03bcd9fa121eb8859369667ee
Simon Smith <me@simonsmith.io>
2015-02-24 13:07:57 +0000

    Remove JSHint failure in gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,6 @@ gulp.task('default', function() {
   return gulp.src('src/herotabs.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'))
     .pipe(webpack({
       output: {
         libraryTarget: 'umd'
