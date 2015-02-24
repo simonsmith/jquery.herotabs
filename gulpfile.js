a5aef5cc688295f2b81482f839c6ec59e5124d85
Simon Smith <me@simonsmith.io>
2015-02-24 11:04:09 +0000

    Use webpack to build the plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var jshint = require('gulp-jshint');
-var rename = require('gulp-rename');
-var header = require('gulp-header');
-require('gulp-grunt')(gulp);
+var gulp =       require('gulp');
+var uglify =     require('gulp-uglify');
+var jshint =     require('gulp-jshint');
+var webpack =    require('gulp-webpack');
+var rename =     require('gulp-rename');
+var header =     require('gulp-header');
+                 require('gulp-grunt')(gulp);
 
 var pkg = require('./package.json');
 var banner = [
@@ -21,6 +22,19 @@ gulp.task('default', function() {
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'))
+    .pipe(webpack({
+      output: {
+        libraryTarget: 'umd'
+      },
+      externals: {
+        'jquery': {
+          commonjs: 'jquery',
+          commonjs2: 'jquery',
+          amd: 'jquery',
+          root: 'jQuery'
+        }
+      }
+    }))
     .pipe(rename('jquery.herotabs.js'))
     .pipe(header(banner, {pkg: pkg}))
     .pipe(gulp.dest('dist'))
