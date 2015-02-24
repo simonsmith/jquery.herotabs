be145b6b0e4c8af63e6133a5d68c2ebc3a8aa138
Simon Smith <me@simonsmith.io>
2015-02-24 08:54:18 +0000

    Update bower packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         src: 'dist/jquery.herotabs.js',
         options: {
           vendor: [
-            'bower_components/jquery/jquery.js',
+            'bower_components/jquery/dist/jquery.js',
             'bower_components/modernizr/modernizr.js',
             'bower_components/jasmine-jquery/lib/jasmine-jquery.js'
           ],
